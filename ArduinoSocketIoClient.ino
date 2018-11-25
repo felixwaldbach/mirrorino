2dc6f048ef3202b850e833e117e34752fd4fa3ee
Emre Besogul <emre@Emres-MacBook-Pro.local>
2018-11-25 14:22:23 +0100

    Add esp8266 connection

--- ArduinoSocketIoClient.ino
+++ ArduinoSocketIoClient.ino
@@ -21,11 +21,16 @@
 //Libraries
 #include <DHT.h>;
 #include <ArduinoJson.h>
+#include <SoftwareSerial.h>
 
 //Constants
 #define DHTPIN 2     // what pin we're connected to
 #define DHTTYPE DHT22   // DHT 22  (AM2302)
+#define RX 10
+#define TX 11
+#define ESP8266
 DHT dht(DHTPIN, DHTTYPE); //// Initialize DHT sensor for normal 16mhz Arduino
+SoftwareSerial esp8266(RX,TX);
 
 //Variables and Objects
 int chk;
@@ -33,12 +38,28 @@ float hum;  //Stores humidity value
 float temp; //Stores temperature value
 String data;
 
+String AP = "xy";       // CHANGE ME
+String PASS = "xy"; // CHANGE ME
+int countTrueCommand;
+int countTimeCommand;
+boolean found = false;
+
+String HOST = "192.168.1.12";
+String PORT = "5000";
+
 StaticJsonBuffer<200> jsonBuffer;
 JsonObject& root = jsonBuffer.createObject();
+
 
 void setup()
 {
   Serial.begin(9600);
+
+  esp8266.begin(115200);
+  sendCommand("AT",5,"OK");
+  sendCommand("AT+CWMODE=1",5,"OK");
+  sendCommand("AT+CWJAP=\""+ AP +"\",\""+ PASS +"\"",20,"OK");
+
   dht.begin();
 }
 
@@ -71,12 +92,43 @@ void loop()
       //Print JSON to serial monitor
       Serial.println(data);
 
-
-      // Send data via ESP8266 and WebSockets to UI
+      // Send data via ESP8266
 
     }
 
     delay(10000); // Delay x sec. for next round
 }
 
-   
+void sendCommand(String command, int maxTime, char readReplay[]) {
+  Serial.print(countTrueCommand);
+  Serial.print(". at command => ");
+  Serial.print(command);
+  Serial.print(" ");
+  while(countTimeCommand < (maxTime*1))
+  {
+    esp8266.println(command);//at+cipsend
+    if(esp8266.find(readReplay))//ok
+    {
+      found = true;
+      break;
+    }
+
+    countTimeCommand++;
+  }
+
+  if(found == true)
+  {
+    Serial.println("Success");
+    countTrueCommand++;
+    countTimeCommand = 0;
+  }
+
+  if(found == false)
+  {
+    Serial.println("Fail");
+    countTrueCommand = 0;
+    countTimeCommand = 0;
+  }
+
+  found = false;
+ }
