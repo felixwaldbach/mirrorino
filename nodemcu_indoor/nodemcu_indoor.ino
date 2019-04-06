6b6918515cf87200731503a8935b003c6745b718
Felix <felix.waldbach@outlook.com>
2019-04-06 15:30:13 +0200

    Renamed WiFi credentials to a static setup for testing purposes

--- nodemcu_indoor/nodemcu_indoor.ino
+++ nodemcu_indoor/nodemcu_indoor.ino
@@ -43,9 +43,9 @@ SimpleTimer pirTimer;
 
 // Update these with values suitable for your network.
 
-const char* ssid = "???";
-const char* password = "???";
-const char* mqtt_server = "???";
+const char* ssid = "SmartMirror";
+const char* password = "smartmirror2019";
+const char* mqtt_server = "192.168.2.104";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -176,6 +176,7 @@ void setup() {
 
   dht.begin();
   dhtTimer.setInterval(1800500, readDht);
+  Serial.println("Calibrating...");
   delay(60000);
   Serial.println("Motion detection ready");
   pirTimer.setInterval(1000, readPir);
--- nodemcu_outdoor/nodemcu_outdoor.ino
+++ nodemcu_outdoor/nodemcu_outdoor.ino
@@ -39,9 +39,9 @@ SimpleTimer dhtTimer;
 
 // Update these with values suitable for your network.
 
-const char* ssid = "???";
-const char* password = "???";
-const char* mqtt_server = "???";
+const char* ssid = "SmartMirror";
+const char* password = "smartmirror2019";
+const char* mqtt_server = "192.168.2.104";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
